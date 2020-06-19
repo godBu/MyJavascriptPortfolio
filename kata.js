dfce14fb62cf2e6805ec608ad4b5e087eaf14352
Ebuka Mbata <rockymmpr@gmail.com>
2020-06-19 21:21:53 +0100

    kata one done

---
+++ kata.js
@@ -0,0 +1,9 @@
+const squareNumber = (number) => {
+    return Number.isInteger(Math.sqrt(number));
+}
+
+console.log(squareNumber(50));
+// Number an abject that represents number of any kind
+// isInteger returns true if the value passed is an integer
+// Math lets us use certain math constants and carry out certain maths functions 
+// returns the square root of a number  
