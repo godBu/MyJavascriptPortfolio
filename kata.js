6e605541d311f190e1aac845112854d83d1a55de
Ebuka Mbata <rockymmpr@gmail.com>
2020-06-20 19:00:05 +0100

    alt solve

--- kata.js
+++ kata.js
@@ -3,6 +3,17 @@ const squareNumber = (number) => {
 }
 
 console.log(squareNumber(50));
+
+// const squareNumber = (number) => {
+//     for (i = 1; i*i <= number; i++){
+//         if (i*i == number)
+//         return true;
+//     }
+//     return false;
+// }
+
+// console.log(squareNumber(400));
+
 // Number an abject that represents number of any kind
 // isInteger returns true if the value passed is an integer
 // Math lets us use certain math constants and carry out certain maths functions 
